29604c5a9b930759a9063f5b3b172fb9bc229100
haglass <haglass@hanmail.net>
2023-01-20 18:08:48 +0900

    주문표 전체 삭제 수정중

--- src/components/about_p/ListMenu.js
+++ src/components/about_p/ListMenu.js
@@ -65,35 +65,40 @@ const ListMenu = ({ increaseQ, decreaseQ, cartCountTotal, removeFromCart }) => {
         </div>
       </div>
       <div className=" w-2/6 pt-6 rounded-md">
-        <div className="flex justify-center justify-content: center">
-          <div className="block rounded-lg shadow-lg bg-white text-white max-w-sm text-center">
-            <div className="py-3 px-6 rounded-md bg-red-500 ">주문표</div>
-            <div className="p-6">
-              <h5 className="text-gray-900 text-xl font-medium mb-2">
-                주문표에 담긴 메뉴가 없습니다.
-              </h5>
-              <p className="text-gray-700 text-base mb-4"></p>
-              <div className="py-3 px-6 border-t flex justify-between border-gray-300 text-gray-600">
-                배달요금 <span>3,000원</span>
-              </div>
-              <div className="py-3 px-6 border-t border-b flex justify-between border-gray-300 text-gray-600">
-                총 합계 <span>21,200원</span>
-              </div>
+        <div className="block rounded-lg shadow-lg bg-white text-white max-w-sm text-center">
+          <div className=" flex justify-between bg-red-500 ">
+            <p className="p-3  py-2.5 "> 주문표 </p>
+            <img
+              src="photo/휴지통.png"
+              className="scale-50 "
+              alt="제품사진"
+            ></img>
+          </div>
+          <div className="p-6">
+            <h5 className="text-gray-900 text-xl font-medium mb-2">
+              주문표에 담긴 메뉴가 없습니다.
+            </h5>
+
+            <div className="py-3 px-6 border-t flex justify-between border-gray-300 text-gray-600">
+              배달요금 <span>3,000원</span>
             </div>
-            HTML
-            <Link to="/shoppingbag">
-              <button
-                type="button"
-                className="mb-2 w-full inline-block px-6 py-2.5
+            <div className="py-3 px-6 border-t border-b flex justify-between border-gray-300 text-gray-600">
+              총 합계 <span>21,200원</span>
+            </div>
+          </div>
+          HTML
+          <Link to="/shoppingbag">
+            <button
+              type="button"
+              className=" w-full inline-block px-6 py-2.5
                bg-red-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md
                 hover:bg-red-700 hover:shadow-lg
                  focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0
                active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
-              >
-                장바구니로 이동하기
-              </button>
-            </Link>
-          </div>
+            >
+              장바구니로 이동하기
+            </button>
+          </Link>
         </div>
       </div>
     </div>
