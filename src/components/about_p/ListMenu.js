b6c42e680db331d05bb92f7b30c6f68adc1c1d84
haglass <haglass@hanmail.net>
2023-01-25 18:12:16 +0900

    주문표 컴포넌트 분리

--- src/components/about_p/ListMenu.js
+++ src/components/about_p/ListMenu.js
@@ -4,20 +4,13 @@ import { Link } from "react-router-dom";
 import ListMenuMenu from "./ListMenuMenu";
 import ListReview from "./ListReview";
 import StoreInfo from "./StoreInfo";
+import Oder from "./Order";
 
 const ListMenu = ({ increaseQ, decreaseQ, cartCountTotal, removeFromCart }) => {
   // 0은 List 메뉴
   // 1은 상세메뉴
   // 2는 정보메뉴
   const [showType, setShowType] = useState(0);
-  const onRemove = () => {
-    if (window.confirm("주문 메뉴를 모두 삭제하시겠습니까?")) {
-      alert("삭제되었습니다.");
-    } else {
-      alert("취소합니다.");
-    }
-  };
-  useEffect(() => {}, [showType]);
 
   // 외부 서버 데이터 연동
   // useEffec 를 이용해서 최초에 데이터를 가지고 온다.
@@ -90,48 +83,8 @@ const ListMenu = ({ increaseQ, decreaseQ, cartCountTotal, removeFromCart }) => {
             </div>
           </div>
         ))}
-
       </div>
-      <div className=" w-2/6 pt-6 rounded-md">
-        <div className="block rounded-lg shadow-lg bg-white text-white max-w-sm text-center">
-          <div className=" flex justify-between bg-red-500 ">
-            <p className="p-3  py-2.5 "> 주문표 </p>
-
-            <button onClick={onRemove}>
-              <img
-                src="photo/휴지통.png"
-                className="scale-50 "
-                alt="제품사진"
-              ></img>
-            </button>
-          </div>
-          <div className="p-6">
-            <h5 className="text-gray-900 text-xl font-medium mb-2">
-              주문표에 담긴 메뉴가 없습니다.
-            </h5>
-
-            <div className="py-3 px-6 border-t flex justify-between border-gray-300 text-gray-600">
-              배달요금 <span>3,000원</span>
-            </div>
-            <div className="py-3 px-6 border-t border-b flex justify-between border-gray-300 text-gray-600">
-              총 합계 <span>21,200원</span>
-            </div>
-          </div>
-          HTML
-          <Link to="/shoppingbag">
-            <button
-              type="button"
-              className=" w-full inline-block px-6 py-2.5
-               bg-red-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md
-                hover:bg-red-700 hover:shadow-lg
-                 focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0
-               active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              장바구니로 이동하기
-            </button>
-          </Link>
-        </div>
-      </div>
+      <Oder />
     </div>
   );
 };
--- src/components/modal_p/DeliveryInfo.js
+++ src/components/modal_p/DeliveryInfo.js
@@ -102,12 +102,14 @@ const DeliveryInfo = ({ setIsShowing, item, carts, setCarts }) => {
           className=" w-full  
                bg-blue-300"
         >
-          <button
-            type="button"
-            className=" inline-block w-1/2 px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            주문표에 추가
-          </button>
+          <Link to="/Order">
+            <button
+              type="button"
+              className=" inline-block w-1/2 px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              주문표에 추가
+            </button>
+          </Link>
           <Link to="/buy">
             <button
               type="button"
