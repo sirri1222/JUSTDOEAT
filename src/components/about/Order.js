c1473474944962d590ddf304aa1172f0426954ad
haglass <haglass@hanmail.net>
2023-02-03 10:27:41 +0900

    ...

--- src/components/about/Order.js
+++ src/components/about/Order.js
@@ -46,9 +46,6 @@ const Order = () => {
             )}
           </div>
         </div>
-        <span className="py-3 px-6  flex justify-between text-gray-600">
-          배달요금 <span>3,000원</span>
-        </span>
 
         <span className="py-3 px-6  flex justify-between text-red-500">
           총 합계 <span>21,200원</span>
